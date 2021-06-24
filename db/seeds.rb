ae5dbc3d14098897baa75153d4e102bcabf3cc57
RyoKitatani <ryo0929m@gmail.com>
2021-06-24 18:23:53 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "create Users"
+
+User.create!(
+  email: "user1@example.com",
+  password: "123456",
+  name: "オリバー",
+  self_introduction: "こんにちは",
+  sex: 0,
+  img_name: open("#{Rails.root}/db/dummy_img/1.jpg")
+)
+
+User.create!(
+  email: "user2@example.com",
+  password: "123456",
+  name: "ハリー",
+  self_introduction: "こんにちは",
+  sex: 0,
+  img_name: open("#{Rails.root}/db/dummy_img/2.jpg")
+)
+
+User.create!(
+  email: "user3@example.com",
+  password: "123456",
+  name: "リリー",
+  self_introduction: "こんにちは",
+  sex: 0,
+  img_name: open("#{Rails.root}/db/dummy_img/3.jpg")
+)
